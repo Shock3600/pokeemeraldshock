112192d084a074f093d7d6a65bd5e913e8916712
Shock3600 <dalston.levi.jones@gmail.com>
2021-12-09 15:35:38 -0600

    Hopefully fixed Birch giving you a stone?

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -97,7 +97,7 @@
 #define FLAG_LEGENDARIES_IN_SOOTOPOLIS           0x53
 
 #define FLAG_MEGA_BRACELET_GET               0x54  // Mega bracelet flag used by Shock
-#define FLAG_UNUSED_0x055                    0x55  // Unused Flag
+#define FLAG_BIRCH_STONE_GET                 0x55  // Gotten megastone from Birch
 
 #define FLAG_HIDE_CONTEST_POKE_BALL          0x56  // Always set after new game, object it hides is added directly
 #define FLAG_MET_RIVAL_MOM                   0x57
